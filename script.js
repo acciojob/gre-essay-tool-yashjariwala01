4cc7134bf2a6e232721199d713cdb6be51f424ff
yashjariwala01 <122197521+yashjariwala01@users.noreply.github.com>
2023-03-05 15:50:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your code here
+function countWord() {
+    var words = document.getElementById("evaluatedText").value;
+          var count = 0;
+          var split = words.split(' ');
+          for (var i = 0; i < split.length; i++) {
+              if (split[i] != "") {
+                  count += 1;
+              }
+          }
+document.getElementById("wordCount").innerHTML = count;
+      }
